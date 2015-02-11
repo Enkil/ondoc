1abdb74d05a0590c88e5ee1510bbcd5e263741b5
Igor Timohin <itimohin@NET-SIMPLE.RU>
2015-02-11 11:36:51 +0300

    http://ondoc.myjetbrains.com/youtrack/issue/B-5
    @1h30min

---
+++ js/modal-toggle.js
@@ -0,0 +1,24 @@
+/*
+Toggle css class to modal window for delete consultation.
+Start by click on "Delete" button
+ */
+
+$(document).ready(function() {
+
+    $('.widget-btn--delete').click(function(event){
+        event.preventDefault();
+        $('.modal-consultation-delete__wrapper').toggleClass('display-none');
+    });
+
+    $('.modal-consultation-delete__close').click(function(event){
+        event.preventDefault();
+        $('.modal-consultation-delete__wrapper').toggleClass('display-none');
+    });
+
+    $('.modal-consultation-delete__btn-no').click(function(event){
+        event.preventDefault();
+        $('.modal-consultation-delete__wrapper').toggleClass('display-none');
+    });
+
+});
+
