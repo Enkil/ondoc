f67ee73ea9e4f8f1bddffccce11216da9c93a2ad
Tatyana <tatyana.simonenko@gmail.com>
2015-02-24 20:36:20 +0400

    b-60

--- src/js/user-notifications.js
+++ src/js/user-notifications.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
+
+	// icons active / not active
+
 	$(".notifications-info span").click(function(){
 		$(this).toggleClass("notifications-info__active");
 	});
-});
+
+
+	// lets open modal time zone
+
+	$(".notifications-delivery__umt").click(function(event){
+		event.preventDefault();
+		$(this).parent().parent().find(".notifications-modal").removeClass("display-none");
+	});
+
+
+	// lets close modal
+	
+	$(".notifications-modal__close").click(function(event){
+		event.preventDefault();
+    	$(this).parent().parent().parent().parent().find(".notifications-modal").addClass("display-none");
+    });
+
+
+    // lets close modal after choosing time
+
+    $(".notifications-modal__zone-item").click(function(event){
+    	event.preventDefault();
+    	$(this).parent().parent().parent().parent().parent().parent().parent().find(".notifications-modal").addClass("display-none");
+    });
+}); 
