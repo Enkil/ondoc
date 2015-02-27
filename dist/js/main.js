b9bd56f3e0008009864c2786b5e4b176e8460b90
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-02-27 08:51:28 +0300

    B-27 (in progress) - drugs.html fully reworked
    7h

--- dist/js/main.js
+++ dist/js/main.js
@@ -11553,7 +11553,39 @@ $(document).ready(function() {
 
 });
 
+/*
+ Toogle doctor info pop-up
+ */
+$(document).ready(function() {
 
+    $('.drugs__doctor-name').click(function(event){
+        event.preventDefault();
+        $('.medcard-docinfo').toggleClass('display-none');
+    });
+
+    $('.js-doc-find').click(function(event){
+        event.preventDefault();
+        $('.medcard-docinfo').toggleClass('display-none');
+    });
+
+});
+
+/*
+* Toogle reaction popup
+*/
+$(document).ready(function() {
+
+    $('.js-drugs-popup-toogle').mouseover(function(event){
+        event.preventDefault();
+        $('.drugs__drug-reaction-popup').toggleClass('display-none');
+    });
+
+    $('.js-drugs-popup-toogle').click(function(event){
+        event.preventDefault();
+        $('.drugs__drug-reaction-popup').toggleClass('display-none');
+    });
+
+});
 /*
  Toggle display: none; for header dropdown
  */
---
+++ src/js/drugs.js
@@ -0,0 +1,33 @@
+/*
+ Toogle doctor info pop-up
+ */
+$(document).ready(function() {
+
+    $('.drugs__doctor-name').click(function(event){
+        event.preventDefault();
+        $('.medcard-docinfo').toggleClass('display-none');
+    });
+
+    $('.js-doc-find').click(function(event){
+        event.preventDefault();
+        $('.medcard-docinfo').toggleClass('display-none');
+    });
+
+});
+
+/*
+* Toogle reaction popup
+*/
+$(document).ready(function() {
+
+    $('.js-drugs-popup-toogle').mouseover(function(event){
+        event.preventDefault();
+        $('.drugs__drug-reaction-popup').toggleClass('display-none');
+    });
+
+    $('.js-drugs-popup-toogle').click(function(event){
+        event.preventDefault();
+        $('.drugs__drug-reaction-popup').toggleClass('display-none');
+    });
+
+});
