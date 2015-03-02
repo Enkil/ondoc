0942bf6f40c9580ec27b52b7e3af9fb57085db2e
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-02 10:16:40 +0300

    B-41 5h

--- dist/js/main.js
+++ dist/js/main.js
@@ -11554,6 +11554,19 @@ $(document).ready(function() {
 });
 
 /*
+Toggle search input display none
+ */
+
+$(document).ready(function() {
+
+    $('.clinics__search').click(function(event){
+        event.preventDefault();
+        $('.js__search-form').toggleClass('display-none');
+        $(this).toggleClass('display-none');
+    });
+
+});
+/*
  Toggle doctor info pop-up
  */
 $(document).ready(function() {
