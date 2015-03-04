a6cc7aafe70069a18b24d177227afbb26e122365
Tatyana <tatyana.simonenko@gmail.com>
2015-03-04 23:32:39 +0400

    b-62

--- dist/js/main.js
+++ dist/js/main.js
@@ -11565,6 +11565,23 @@ $(document).ready(function() {
         $(this).toggleClass('display-none');
     });
 
+    // lets open modal
+	
+	$(".clinics__enroll-link").click(function(event){
+		event.preventDefault();
+    	$(this).parent()
+    	.find(".clinics-modal").removeClass("display-none");
+    });
+
+
+    // lets close modal
+	
+	$(".clinics-modal__close").click(function(event){
+		event.preventDefault();
+    	$(this).parent().parent().parent().parent().parent().parent()
+    	.find(".clinics-modal").addClass("display-none");
+    });
+
 });
 /*
  Toggle search input display none
--- src/js/clinics.js
+++ src/js/clinics.js
@@ -10,4 +10,21 @@ $(document).ready(function() {
         $(this).toggleClass('display-none');
     });
 
+    // lets open modal
+	
+	$(".clinics__enroll-link").click(function(event){
+		event.preventDefault();
+    	$(this).parent()
+    	.find(".clinics-modal").removeClass("display-none");
+    });
+
+
+    // lets close modal
+	
+	$(".clinics-modal__close").click(function(event){
+		event.preventDefault();
+    	$(this).parent().parent().parent().parent().parent().parent()
+    	.find(".clinics-modal").addClass("display-none");
+    });
+
 });
