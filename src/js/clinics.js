a0b9871ef4ef37ed361637f3afcf6bd068be93ec
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-06 03:22:43 +0300

    B-40 2h30min

--- src/js/clinics.js
+++ src/js/clinics.js
@@ -11,3 +11,28 @@ $(document).ready(function() {
     });
 
 });
+
+/*
+Toogle heart checkbox
+*/
+$(document).ready(function() {
+
+    $('.filter__check-round').click(function(event){
+        event.preventDefault();
+        $(this).toggleClass('filter__check-round--active');
+    });
+
+});
+
+/*
+Toogle filter
+ */
+$(document).ready(function() {
+
+    $('.clinics__head-link--filter').click(function(event){
+        event.preventDefault();
+        $('.medcard-filter').toggleClass('display-none');
+        $(this).toggleClass('arrow-up');
+    });
+
+});
