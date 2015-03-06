4d972ac6ad5000ee1de3ad2f3f431dc1036bd306
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-06 05:49:43 +0300

    B-24 3h

--- src/js/docs.js
+++ src/js/docs.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
         $(this).toggleClass('display-none');
     });
 
+    $('.docs__head-link--filter').click(function(event){
+        event.preventDefault();
+        $('.medcard-filter').toggleClass('display-none');
+        $(this).toggleClass('arrow-up');
+    });
+
     // lets open modal
 	
 	$(".docs__enroll-link").click(function(event){
@@ -25,6 +31,35 @@ $(document).ready(function() {
 		event.preventDefault();
     	$(this).parent().parent().parent().parent().parent().parent()
     	.find(".docs-modal").addClass("display-none");
+    });
+
+});
+
+/*
+Range slider
+ */
+
+$(document).ready(function() {
+
+    var $range = $("#filter__range");
+
+    $range.ionRangeSlider({
+        type: "double",
+        min: 1,
+        max: 50,
+        from: 3,
+        to: 25,
+        hide_min_max: true,
+        hide_from_to: true,
+        grid: false
+    });
+
+    $range.on("change", function () {
+        var $this = $(this),
+            from = $this.data("from"),
+            to = $this.data("to");
+        $('#filter__rank-from').html(from);
+        $('#filter__rank-to').html(to);
     });
 
 });
