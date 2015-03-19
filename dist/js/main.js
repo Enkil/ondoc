642c20888d671dbbf93b7f0c543d94ef8fe4cd16
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-20 00:54:08 +0300

    B-218 1h30m

--- dist/js/main.js
+++ dist/js/main.js
@@ -17015,3 +17015,61 @@ $(window).on('scroll', function(){
         $('#js__animate-7').addClass('fdc-section__devices--active');
     }
 });
+google.maps.event.addDomListener(window, 'load', init);
+var map;
+function init() {
+    var mapOptions = {
+        center: new google.maps.LatLng(59.9401434,30.2600572),
+        zoom: 15,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.DEFAULT,
+        },
+        disableDoubleClickZoom: true,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        },
+        scaleControl: true,
+        scrollwheel: false,
+        panControl: true,
+        streetViewControl: true,
+        draggable : true,
+        overviewMapControl: true,
+        overviewMapControlOptions: {
+            opened: false,
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    }
+    var mapElement = document.getElementById('ondoc-on-map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var locations = [
+        ['OnDoc', 'undefined', '+7 800 775-73-96', 'undefined', 'https://ondoc.me', 59.9401434, 30.2600572, 'https://mapbuildr.com/assets/img/markers/default.png']
+    ];
+    for (i = 0; i < locations.length; i++) {
+        if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+        if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+        if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+        if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+        if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+        marker = new google.maps.Marker({
+            icon: markericon,
+            position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+            map: map,
+            title: locations[i][0],
+            desc: description,
+            tel: telephone,
+            email: email,
+            web: web
+        });
+        link = '';     }
+
+
+    var contentString = '<div id="ondoc-on-map__content">OnDoc - система персонального здравоохранения, включающая в себя сервисы для надежного хранения персональной медицинской информации, ее комплексного анализа, и выявления лучшего способа укрепить и сохранить здоровье.</div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+}
