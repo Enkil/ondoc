f10381d228cd0807f85c52fc6914bf1a21e8e4c5
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-28 11:22:51 +0300

    B-146 4h

--- dist/js/main.js
+++ dist/js/main.js
@@ -17058,6 +17058,21 @@ $(document).ready(function () {
 
 });
 
+/*  Scripts for Datepicker popup    */
+
+/*  Change date state   */
+$('.datepicker__table-item').click(function (event) {
+    event.preventDefault();
+    $(this).parents('.datepicker').find('.datepicker__table-item').removeClass('datepicker__table-item--active');
+    //$('.datepicker__table-item').removeClass('datepicker__table-item--active');
+    $(this).addClass('datepicker__table-item--active');
+});
+
+$('.datepicker__btn--ok, .datepicker__btn').click(function (event) {
+    event.preventDefault();
+    $(this).parents('.datepicker').addClass('display-none');
+});
+
 /* Toggle doctor info pop-up */
 $(document).ready(function () {
 
