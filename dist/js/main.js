206f89e2d6fa229150491859e0cbeeb8899d4e94
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-30 17:54:04 +0300

    B-262 15m

--- dist/js/main.js
+++ dist/js/main.js
@@ -17058,21 +17058,6 @@ $(document).ready(function () {
 
 });
 
-/*  Scripts for Datepicker popup    */
-
-/*  Change date state   */
-$('.datepicker__table-item').click(function (event) {
-    event.preventDefault();
-    $(this).parents('.datepicker').find('.datepicker__table-item').removeClass('datepicker__table-item--active');
-    //$('.datepicker__table-item').removeClass('datepicker__table-item--active');
-    $(this).addClass('datepicker__table-item--active');
-});
-
-$('.datepicker__btn--ok, .datepicker__btn').click(function (event) {
-    event.preventDefault();
-    $(this).parents('.datepicker').addClass('display-none');
-});
-
 /* Toggle doctor info pop-up */
 $(document).ready(function () {
 
@@ -17169,4 +17154,18 @@ $(document).ready(function () {
         $('.js__find--textarea-for-err').toggleClass('widget__textarea--error');
         $('.widget__textarea-wrapper').toggleClass('widget__textarea-wrapper--asterisk');
     });
+});
+/*  Scripts for Datepicker popup    */
+
+/*  Change date state   */
+$('.datepicker__table-item').click(function (event) {
+    event.preventDefault();
+    $(this).parents('.datepicker').find('.datepicker__table-item').removeClass('datepicker__table-item--active');
+    //$('.datepicker__table-item').removeClass('datepicker__table-item--active');
+    $(this).addClass('datepicker__table-item--active');
+});
+
+$('.datepicker__btn--ok, .datepicker__btn').click(function (event) {
+    event.preventDefault();
+    $(this).parents('.datepicker').addClass('display-none');
 });
