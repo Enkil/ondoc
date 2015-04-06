773ce87d4c6776833045bd2f46b02d9a8b13348b
Tatyana <tatyana.simonenko@gmail.com>
2015-04-06 12:54:38 +0400

    b-38 vozmozhnosti_1.html

--- dist/js/main.js
+++ dist/js/main.js
@@ -16888,111 +16888,6 @@ e.series.length);d--;)i=b.series[d]||{},b.series[d]=g.merge(i,e.series[d]);else 
 c?f.push(e):f[d.configName]=e});if(this.name===void 0&&this.readers.length>=2&&(e=this.getReferencedColumnIndexes(),e.length>=2))e.shift(),e.sort(),this.name=a[e.shift()].name;return f};l.prototype.addColumnReader=function(a,b){this.readers.push({columnIndex:a,configName:b});if(!(b==="x"||b==="y"||b===void 0))this.pointIsArray=!1};l.prototype.getReferencedColumnIndexes=function(){var a,b=[],c;for(a=0;a<this.readers.length;a+=1)c=this.readers[a],c.columnIndex!==void 0&&b.push(c.columnIndex);return b};
 l.prototype.hasReader=function(a){var b,c;for(b=0;b<this.readers.length;b+=1)if(c=this.readers[b],c.configName===a)return!0}})(Highcharts);
 
-/* Toggle doctor info pop-up */
-$(document).ready(function() {
-
-    $('.drugs__doctor-name').click(function(event){
-        event.preventDefault();
-        $('.medcard-docinfo').toggleClass('display-none');
-    });
-
-    $('.js-doc-find').click(function(event){
-        event.preventDefault();
-        $('.medcard-docinfo').toggleClass('display-none');
-    });
-
-});
-
-/* Toggle reaction popup */
-$(document).ready(function() {
-
-    $('.js-drugs-popup-toogle').mouseover(function(event){
-        event.preventDefault();
-        $('.drugs__drug-reaction-popup').toggleClass('display-none');
-    });
-
-    $('.js-drugs-popup-toogle').click(function(event){
-        event.preventDefault();
-        $('.drugs__drug-reaction-popup').toggleClass('display-none');
-    });
-
-});
-
-/* Toggle dosage types and items popups */
-$(document).ready(function() {
-
-    $('.drugs__link--type').click(function(event){
-        event.preventDefault();
-        $(this).next().toggleClass('display-none');
-    });
-
-    $('.drugs__link--item').click(function(event){
-        event.preventDefault();
-        $(this).next().toggleClass('display-none');
-    });
-
-    $('.js__find-add-drug').click(function(event){
-        event.preventDefault();
-        $('.drugs__modal-wrapper').removeClass('display-none')
-    });
-
-    $('.drugs__modal-close').click(function(event){
-        event.preventDefault();
-        $('.drugs__modal-wrapper').addClass('display-none')
-    });
-
-});
-
-/* Toggle days state */
-$(document).ready(function() {
-
-    $('.js__drug-day').click(function(event){
-        event.preventDefault();
-        $(this).toggleClass('drugs__drug-day--active');
-    });
-});
-
-/* Toggle doc select display */
-$(document).ready(function() {
-
-    $('.js__drugs-doc-select').click(function(event){
-        event.preventDefault();
-        $('body').toggleClass('popup-scrolltop');
-        $('.medcard-doctor__wrapper').toggleClass('display-none');
-    });
-});
-
-/* Show/Hide form errors */
-$(document).ready(function () {
-
-    $('.widget-btn--submit-yes').click(function (event) {
-        event.preventDefault();
-        $('.drugs-errors').toggleClass('display-none');
-        $('.drugs__drug-comment').toggleClass('drugs__drug-comment--error');
-        $('.drugs__link').toggleClass('drugs__link--error');
-        $('.drugs__dosage-wrapper').toggleClass('drugs__dosage-wrapper--asterisk');
-        $('.drugs__dosage').toggleClass('drugs__dosage--error');
-        $('.js__drug-day').not('.drugs__drug-day--active').toggleClass('drugs__drug-day--error');
-    });
-});
-/* Show/Hide form errors */
-
-$(document).ready(function () {
-
-    $('.widget-btn--submit-yes').click(function (event) {
-        event.preventDefault();
-        $('.medcard-errors').toggleClass('display-none');
-        $('.js__find--textarea-for-err').toggleClass('widget__textarea--error');
-        $('.widget__textarea-wrapper').toggleClass('widget__textarea-wrapper--asterisk');
-    });
-});
-$(document).ready(function () {
-
-    $('.operator-tasks-filer__value').click(function (event) {
-        event.preventDefault();
-        $(this).children('.operator-tasks-filer__dropdown').toggleClass('display-none')
-    });
-});
 /* Toggle promo form */
 
 $(document).ready(function() {
@@ -17171,6 +17066,111 @@ $(document).ready(function () {
 
 });
 
+/* Toggle doctor info pop-up */
+$(document).ready(function() {
+
+    $('.drugs__doctor-name').click(function(event){
+        event.preventDefault();
+        $('.medcard-docinfo').toggleClass('display-none');
+    });
+
+    $('.js-doc-find').click(function(event){
+        event.preventDefault();
+        $('.medcard-docinfo').toggleClass('display-none');
+    });
+
+});
+
+/* Toggle reaction popup */
+$(document).ready(function() {
+
+    $('.js-drugs-popup-toogle').mouseover(function(event){
+        event.preventDefault();
+        $('.drugs__drug-reaction-popup').toggleClass('display-none');
+    });
+
+    $('.js-drugs-popup-toogle').click(function(event){
+        event.preventDefault();
+        $('.drugs__drug-reaction-popup').toggleClass('display-none');
+    });
+
+});
+
+/* Toggle dosage types and items popups */
+$(document).ready(function() {
+
+    $('.drugs__link--type').click(function(event){
+        event.preventDefault();
+        $(this).next().toggleClass('display-none');
+    });
+
+    $('.drugs__link--item').click(function(event){
+        event.preventDefault();
+        $(this).next().toggleClass('display-none');
+    });
+
+    $('.js__find-add-drug').click(function(event){
+        event.preventDefault();
+        $('.drugs__modal-wrapper').removeClass('display-none')
+    });
+
+    $('.drugs__modal-close').click(function(event){
+        event.preventDefault();
+        $('.drugs__modal-wrapper').addClass('display-none')
+    });
+
+});
+
+/* Toggle days state */
+$(document).ready(function() {
+
+    $('.js__drug-day').click(function(event){
+        event.preventDefault();
+        $(this).toggleClass('drugs__drug-day--active');
+    });
+});
+
+/* Toggle doc select display */
+$(document).ready(function() {
+
+    $('.js__drugs-doc-select').click(function(event){
+        event.preventDefault();
+        $('body').toggleClass('popup-scrolltop');
+        $('.medcard-doctor__wrapper').toggleClass('display-none');
+    });
+});
+
+/* Show/Hide form errors */
+$(document).ready(function () {
+
+    $('.widget-btn--submit-yes').click(function (event) {
+        event.preventDefault();
+        $('.drugs-errors').toggleClass('display-none');
+        $('.drugs__drug-comment').toggleClass('drugs__drug-comment--error');
+        $('.drugs__link').toggleClass('drugs__link--error');
+        $('.drugs__dosage-wrapper').toggleClass('drugs__dosage-wrapper--asterisk');
+        $('.drugs__dosage').toggleClass('drugs__dosage--error');
+        $('.js__drug-day').not('.drugs__drug-day--active').toggleClass('drugs__drug-day--error');
+    });
+});
+/* Show/Hide form errors */
+
+$(document).ready(function () {
+
+    $('.widget-btn--submit-yes').click(function (event) {
+        event.preventDefault();
+        $('.medcard-errors').toggleClass('display-none');
+        $('.js__find--textarea-for-err').toggleClass('widget__textarea--error');
+        $('.widget__textarea-wrapper').toggleClass('widget__textarea-wrapper--asterisk');
+    });
+});
+$(document).ready(function () {
+
+    $('.operator-tasks-filer__value').click(function (event) {
+        event.preventDefault();
+        $(this).children('.operator-tasks-filer__dropdown').toggleClass('display-none')
+    });
+});
 /*  Scripts for Datepicker popup    */
 
 /*  Change date state   */
