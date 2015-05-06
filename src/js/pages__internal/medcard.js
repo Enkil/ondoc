ee8038f8feec3ff96a3f5e0f0db00a1f060c3e75
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-05-06 11:27:02 +0300

    B-294

--- src/js/pages__internal/medcard.js
+++ src/js/pages__internal/medcard.js
@@ -9,3 +9,20 @@ $(document).ready(function () {
         $('.widget__textarea-wrapper').toggleClass('widget__textarea-wrapper--asterisk');
     });
 });
+
+/* Show/hide popup in stoma scheme */
+
+$(document).ready(function () {
+
+    var $schemePopup = '<div class="stoma-scheme__popup-body"> <h3 class="stoma-scheme__popup-title">До лечения</h3> <span class="stoma-scheme__popup-text">Глубокий кариес с поражением нервов</span> <h3 class="stoma-scheme__popup-title">После лечения</h3> <span class="stoma-scheme__popup-text">Пломба, удалены нервы</span> </div>';
+
+    $('.stoma-scheme__tooth').click(function (event) {
+        event.preventDefault();
+
+        $('.stoma-scheme').find('.stoma-scheme__popup-body').remove();
+        $(this).children('.stoma-scheme__num').after($schemePopup);
+
+    });
+
+
+});
